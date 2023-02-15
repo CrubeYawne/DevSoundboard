6e70ca81c1eb124b906f50b3687b283a20de3e70
owen boyd <owen.boyd@gmail.com>
2023-02-15 16:04:11 -0500

    pushing code

---
+++ SoundBoard/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace SoundBoard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlButtons = new System.Windows.Forms.Panel();
+            this.txtFolderName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.lblCurrentDirectory = new System.Windows.Forms.Label();
+            this.pnlFiles = new System.Windows.Forms.Panel();
+            this.lblFolders = new System.Windows.Forms.Label();
+            this.lblFiles = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlButtons
+            // 
+            this.pnlButtons.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlButtons.AutoScroll = true;
+            this.pnlButtons.Location = new System.Drawing.Point(12, 92);
+            this.pnlButtons.Name = "pnlButtons";
+            this.pnlButtons.Size = new System.Drawing.Size(424, 305);
+            this.pnlButtons.TabIndex = 0;
+            // 
+            // txtFolderName
+            // 
+            this.txtFolderName.AccessibleDescription = "Directory";
+            this.txtFolderName.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.txtFolderName.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.FileSystem;
+            this.txtFolderName.Location = new System.Drawing.Point(12, 12);
+            this.txtFolderName.Name = "txtFolderName";
+            this.txtFolderName.Size = new System.Drawing.Size(614, 20);
+            this.txtFolderName.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(632, 10);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(713, 10);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Stop SFX";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(795, 12);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(54, 20);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // lblCurrentDirectory
+            // 
+            this.lblCurrentDirectory.AutoSize = true;
+            this.lblCurrentDirectory.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblCurrentDirectory.Location = new System.Drawing.Point(12, 50);
+            this.lblCurrentDirectory.Name = "lblCurrentDirectory";
+            this.lblCurrentDirectory.Size = new System.Drawing.Size(86, 13);
+            this.lblCurrentDirectory.TabIndex = 4;
+            this.lblCurrentDirectory.Text = "Current Directory";
+            this.lblCurrentDirectory.Click += new System.EventHandler(this.lblCurrentDirectory_Click);
+            // 
+            // pnlFiles
+            // 
+            this.pnlFiles.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlFiles.AutoScroll = true;
+            this.pnlFiles.Location = new System.Drawing.Point(442, 92);
+            this.pnlFiles.Name = "pnlFiles";
+            this.pnlFiles.Size = new System.Drawing.Size(424, 305);
+            this.pnlFiles.TabIndex = 0;
+            // 
+            // lblFolders
+            // 
+            this.lblFolders.AutoSize = true;
+            this.lblFolders.Location = new System.Drawing.Point(12, 76);
+            this.lblFolders.Name = "lblFolders";
+            this.lblFolders.Size = new System.Drawing.Size(57, 13);
+            this.lblFolders.TabIndex = 5;
+            this.lblFolders.Text = "Directories";
+            // 
+            // lblFiles
+            // 
+            this.lblFiles.AutoSize = true;
+            this.lblFiles.Location = new System.Drawing.Point(439, 76);
+            this.lblFiles.Name = "lblFiles";
+            this.lblFiles.Size = new System.Drawing.Size(28, 13);
+            this.lblFiles.TabIndex = 5;
+            this.lblFiles.Text = "Files";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(873, 409);
+            this.Controls.Add(this.lblFiles);
+            this.Controls.Add(this.lblFolders);
+            this.Controls.Add(this.lblCurrentDirectory);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtFolderName);
+            this.Controls.Add(this.pnlFiles);
+            this.Controls.Add(this.pnlButtons);
+            this.Name = "Form1";
+            this.Text = "Sound Board";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlButtons;
+        private System.Windows.Forms.TextBox txtFolderName;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label lblCurrentDirectory;
+        private System.Windows.Forms.Panel pnlFiles;
+        private System.Windows.Forms.Label lblFolders;
+        private System.Windows.Forms.Label lblFiles;
+    }
+}
+
---
+++ SoundBoard/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoundBoard
+{
+    public partial class Form1 : Form
+    {
+        private Button last_hit;
+        private int button_size = 300;
+        private int height = 25;
+
+        private System.Media.SoundPlayer player;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            numericUpDown1.Value = button_size;
+
+            player = new System.Media.SoundPlayer();
+        }
+
+
+        private void CreateButton(Panel targetDestination, string fname, string text, int idx, Color forec)
+        {
+            Button new_button = new Button();
+            new_button.Click += new_button_Click;
+            new_button.ForeColor = forec;
+            new_button.Tag = fname;
+            new_button.Top = height * idx;
+            new_button.Width = button_size;
+            new_button.Height = height;
+            new_button.TextAlign = ContentAlignment.MiddleRight;
+            new_button.Text = text;
+            targetDestination.Controls.Add(new_button);
+        }
+
+        private void LoadFolder(string fn)
+        {
+
+            if(!System.IO.Directory.Exists(fn))
+            {
+
+                MessageBox.Show("Directory does not exist");
+                return;
+            }
+
+            pnlButtons.Controls.Clear();
+            pnlFiles.Controls.Clear();
+
+            int idx = 0;
+
+            lblCurrentDirectory.Text = fn;
+
+            CreateButton(pnlButtons,Directory.GetParent(fn).FullName, "Parent" , ++idx, Color.Yellow);
+            
+            string[] dirs = Directory.GetDirectories(fn);
+
+            char lastChar = ' ';
+
+            foreach (string f in dirs)
+            {
+                var filename = Path.GetFileName(f);
+
+                CreateButton(pnlButtons, f, filename, ++idx, Color.Green);
+
+            }
+
+            lblFolders.Text = string.Format("Directories: {0}", dirs.Count());
+
+            string[] files = Directory.GetFiles(fn);
+
+            lastChar = ' ';
+
+
+            lblFiles.Text = string.Format("Files: {0}", files.Count());
+
+            foreach (string f in files)
+            {
+                var filename = Path.GetFileName(f);               
+
+                CreateButton(pnlFiles, f, filename, ++idx, Color.Black);
+            }
+
+            
+        }
+
+        private void CreateLabel(Panel targetPanel, string label)
+        {
+            Label lblHeading = new Label();
+            lblHeading.Text = label;
+            
+            targetPanel.Controls.Add(lblHeading);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string fname = txtFolderName.Text;
+
+            if (string.IsNullOrEmpty(fname))
+                MessageBox.Show("No path provided");
+            else
+                LoadFolder(fname);
+                
+        }
+
+        void new_button_Click(object sender, EventArgs e)
+        {
+
+            string tag = (sender as Button).Tag.ToString();
+
+            if (Directory.Exists(tag))
+            {
+                lblCurrentDirectory.Text = tag;
+                LoadFolder(tag);
+            }
+            else
+            {
+                if (last_hit != null)
+                    last_hit.BackColor = Color.Transparent;
+
+
+
+                player.Stop();
+                player.SoundLocation = (sender as Button).Tag.ToString();
+                player.Play();
+
+
+                last_hit = (sender as Button);
+
+                last_hit.BackColor = Color.Yellow;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            player.Stop();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            button_size = (int)numericUpDown1.Value;
+            foreach (Control c in pnlButtons.Controls)
+                c.Width = (int)numericUpDown1.Value;
+        }
+
+        private void lblCurrentDirectory_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.Clipboard.SetText((sender as Label).Text);
+        }
+    }
+}
